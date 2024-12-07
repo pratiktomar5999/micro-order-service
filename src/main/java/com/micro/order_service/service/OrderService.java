300978dd6d7cc519605ee02a72adc8f49da114f5
4ndr01d-pratik <120085895+4ndr01d-pratik@users.noreply.github.com>
2024-12-07 23:50:58 +0530

    feignClient

--- src/main/java/com/micro/order_service/service/OrderService.java
+++ src/main/java/com/micro/order_service/service/OrderService.java
@@ -5,20 +5,28 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.micro.order_service.client.InventoryClient;
 import com.micro.order_service.model.Order;
 import com.micro.order_service.repo.OrderRepo;
 
-@Service
-public class OrderService {
+import lombok.RequiredArgsConstructor;
 
-    @Autowired
+@Service
+@RequiredArgsConstructor
+public class OrderService {
     private OrderRepo orderRepo;
+    private final InventoryClient inventoryClient;
     
     public void placeOrder(Order order){
-        order.setId(null);
-        order.setOrderNumber(UUID.randomUUID().toString());
+        var isInStock = inventoryClient.isInStock(order.getSkuCode(), order.getQuantity());
+        if(isInStock){
+            order.setId(null);
+            order.setOrderNumber(UUID.randomUUID().toString());
 
-        orderRepo.save(order);
+            orderRepo.save(order);
+        }else{
+            throw new RuntimeException("Product with skucode " + order.getSkuCode()+" is not in stock.");
+        }
 
     }
 }
